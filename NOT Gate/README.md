9820710548ab3097e6a2d94644d8edbce6d9314b
Aditya Kulkarni <adityark2603@gmail.com>
2025-06-23 15:06:46 +0530

    Delete NOT Gate directory

--- NOT Gate/README.md
+++
@@ -1,2 +0,0 @@
-## NOT Gate
-NOT gates are commonly used in logic circuits to reverse a signal or create complementary outputs.
--- NOT Gate/design.sv
+++
@@ -1,6 +0,0 @@
-module cmos_inverter (
-    input  logic a,
-    output logic y
-);
-    assign y = ~a;  // Inverter logic
-endmodule
